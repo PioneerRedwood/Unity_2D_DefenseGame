530d042241f8aeabfda2e4ae28a3a18937898d6f
PioneerRedwood <chrisredwood123@gmail.com>
2021-01-28 20:17:29 +0900

    #5-4 Editor

---
+++ 2D_DefenseGame/2D_DefenseGame/Assets/Editor/StageEditor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Stage))]
+public class StageEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        var stage = target as Stage;
+
+        if(GUILayout.Button("타일 생성"))
+        {
+            stage.CreateGrid();
+        }
+
+        if(GUILayout.Button("타일 제거"))
+        {
+            stage.DestroyGrid();
+        }
+    }
+}
---
+++ 2D_DefenseGame/2D_DefenseGame/Assets/Editor/TileEditor.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEditor.Experimental.SceneManagement;
+using UnityEditor.SceneManagement;
+using UnityEditor;
+
+[CustomEditor(typeof(Tile)), CanEditMultipleObjects]
+public class TileEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        var tiles = targets.Cast<Tile>().ToArray();
+        bool bIsValid = false;
+
+        if(GUILayout.Button("컴포넌트 모두 제거"))
+        {
+            bIsValid = true;
+            foreach(var tile in tiles)
+            {
+                tile.RemoveAllTileComponents();
+            }
+        }
+
+        if(GUILayout.Button("평지 추가"))
+        {
+            bIsValid = true;
+            foreach(var tile in tiles)
+            {
+                var route = Instantiate(GamePrefabManager.Instance.routePrefab);
+                var ground = Instantiate(GamePrefabManager.Instance.groundPrefab);
+
+                tile.RemoveTileComponent(route);
+                tile.AddTileComponentEditor(ground);
+            }
+        }
+
+        if(GUILayout.Button("경로 추가"))
+        {
+            bIsValid = true;
+            foreach(var tile in tiles)
+            {
+                var ground = Instantiate(GamePrefabManager.Instance.groundPrefab);
+                var route = Instantiate(GamePrefabManager.Instance.routePrefab);
+
+                tile.RemoveTileComponent(ground);
+                tile.AddTileComponentEditor(route);
+            }
+        }
+
+        if (GUILayout.Button("타워 추가"))
+        {
+            bIsValid = true;
+            foreach(var tile in tiles)
+            {
+                var tower = Instantiate(GamePrefabManager.Instance.towerPrefab);
+                tile.AddTileComponentEditor(tower);
+            }
+        }
+
+        if (bIsValid)
+        {
+            var prefabStage = PrefabStageUtility.GetCurrentPrefabStage();
+            if(prefabStage != null)
+            {
+                EditorSceneManager.MarkSceneDirty(prefabStage.scene);
+            }
+        }
+    }
+}
